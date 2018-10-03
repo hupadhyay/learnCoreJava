54b27a81e9f2a3d9865914d77504607157186983
hupadhyay <himanshu2703@gmail.com>
2018-10-03 14:39:14 -0400

    timout, exception, verify, number of call support to mock is added.

--- src/test/java/in/himtech/java/mock/calc/MathApplicationTest.java
+++ src/test/java/in/himtech/java/mock/calc/MathApplicationTest.java
@@ -6,13 +6,22 @@ import static org.mockito.Mockito.atMost;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.timeout;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.inOrder;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
 
 @RunWith(MockitoJUnitRunner.class)
 public class MathApplicationTest {
@@ -74,6 +83,87 @@ public class MathApplicationTest {
 
 		// check a minimum 1 call count
 		verify(calculatorService, atLeastOnce()).divide(100.0, 20.0);
+
+		//verify call to add method to be completed within 100 ms
+		verify(calculatorService, timeout(100)).divide(100.0, 20.0);
+		
+		// invocation count can be added to ensure multiplication invocations
+		// can be checked within given timeframe
+		verify(calculatorService, timeout(100).times(1)).divide(100.0, 20.0);
+
 	}
+
+	@Test(expected = RuntimeException.class)
+	public void testAgainAdd() {
+		when(calculatorService.add(10.0, 20.0)).thenThrow(new RuntimeException("Addition is not possible."));
+
+		double value = mathApplication.add(10.0, 20.0);
+		Assert.assertEquals(30.0, value, 0.0);
+
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void testExcepAdd() {
+		doThrow(new ArithmeticException("Addition is not possible.")).when(calculatorService.add(10.0, 20.0));
+
+		double value = mathApplication.add(10.0, 20.0);
+		Assert.assertEquals(30.0, value, 0.0);
+	}
+
+	@Test
+	public void testAddAndSubtract() {
+
+		// add the behavior to add numbers
+		when(calculatorService.add(20.0, 10.0)).thenReturn(30.0);
+
+		// subtract the behavior to subtract numbers
+		when(calculatorService.subtract(20.0, 10.0)).thenReturn(10.0);
+
+		// test the add functionality
+		Assert.assertEquals(30.0, mathApplication.add(20.0, 10.0), 0);
+
+		// test the subtract functionality
+		Assert.assertEquals(10.0, mathApplication.subtract(20.0, 10.0), 0);
+
+		// create an inOrder verifier for a single mock
+		InOrder inOrder = inOrder(calculatorService);
+
+		// following will make sure that add is first called then subtract is called.
+		inOrder.verify(calculatorService).add(20.0, 10.0);
+		inOrder.verify(calculatorService).subtract(20.0, 10.0);
+	}
+
+	@Test
+	public void testAnswerAdd() {
+
+		// add the behavior to add numbers
+		when(calculatorService.add(20.0, 10.0)).thenAnswer(new Answer<Double>() {
+
+			@Override
+			public Double answer(InvocationOnMock invocation) throws Throwable {
+				// get the arguments passed to mock
+				Object[] args = invocation.getArguments();
+				System.out.println("method arguments are: " + Arrays.toString(args));
+
+				// get the mock
+				Object mock = invocation.getMock();
+				System.out.println("Mock Object: " + mock.toString());
+
+				// return the result
+				return 30.0;
+			}
+		});
+
+		// test the add functionality
+		Assert.assertEquals(30.0, mathApplication.add(20.0, 10.0), 0);
+	}
+
+	/**
+	 * Mockito provides the capability to a reset a mock so that it can be reused
+	 * later. Take a look at the following code snippet.
+	 * <code>reset(calcService); </code> Here we've reset mock object.
+	 * MathApplication makes use of calcService and after reset the mock, using
+	 * mocked method will fail the test.
+	 */
 
 }
